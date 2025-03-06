47acc903f36e3a15ef109508d41c026467787f6b
swapxs <swapnil.bhowmik@xenonstack.com>
2025-03-06 14:46:13 +0530

    run function redefined for better readability

--- notejam-docker/run.sh
+++ notejam-docker/run.sh
@@ -1,18 +1,22 @@
 #!/bin/sh
 
-build() {
+stop() {
     printf "Stop and remove existing containers\n"
     docker-compose down -v || {
         printf "Failed to stop containers. Check Docker status."
         exit 1
     }
+}
 
+clean() {
     printf "Removing redundant resources\n"
     docker system prune -af || {
         printf "Failed to clean Docker resources."
         exit 1
     }
+}
 
+build() {
     printf "Build and run all docker containers\n"
     docker-compose up --build -d || {
         printf "Failed to build or start containers."
@@ -24,7 +28,7 @@ wait() {
     sleep 10
 }
 
-run_program() {
+sync_and_migrate() {
     printf "Running database migrations\n"
     docker exec -it notejam-app python manage.py syncdb || {
         printf "Failed to run syncdb."
@@ -41,8 +45,10 @@ run_program() {
 
 
 case "$1" in
-    --br) build && run_program ;;
-    --b) build ;;
-    --r) run_program ;;
-    *) printf "Need arguments\nEither use build (--b) or run (--r) or build and run (--br)"
+    --init) stop && clean && build && sync_and_migrate ;;
+    --stop) stop ;;
+    --build) build ;;
+    --clean) clean ;;
+    --sync) sync_and_migrate ;;
+    *) printf "Invalid" ;;
 esac
