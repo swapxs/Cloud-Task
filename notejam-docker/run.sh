bad4afefb39404ccbf2120f2f104e7f50e7596af
swapxs <swapnil.bhowmik@xenonstack.com>
2025-03-06 10:47:06 +0530

    minor output update

--- notejam-docker/run.sh
+++ notejam-docker/run.sh
@@ -1,26 +1,26 @@
 #!/bin/sh
 
-printf "Stop and remove existing containers"
+printf "Stop and remove existing containers\n"
 docker-compose down -v || {
     printf "Failed to stop containers. Check Docker status."
     exit 1
 }
 
-printf "Removing redundant resources"
+printf "Removing redundant resources\n"
 docker system prune -af || {
     printf "Failed to clean Docker resources."
     exit 1
 }
 
-printf "Build and run all docker containers"
+printf "Build and run all docker containers\n"
 docker-compose up --build -d || {
     printf "Failed to build or start containers."
     exit 1
 }
 
-sleep 5
+sleep 10
 
-printf "Running database migrations"
+printf "Running database migrations\n"
 docker exec -it notejam-app python manage.py syncdb || {
     printf "Failed to run syncdb."
     exit 1
@@ -31,4 +31,4 @@ docker exec -it notejam-app python manage.py migrate || {
     exit 1
 }
 
-printf "Application is now running."
+printf "Application is now running.\n"
