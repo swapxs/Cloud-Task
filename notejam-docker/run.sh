81bfdc89e1522271ae8687e5cf73fe2d94ab3627
swapxs <swapnil.bhowmik@xenonstack.com>
2025-03-06 10:29:28 +0530

    simple POSIX script to build and run (needs further improvement)

--- notejam-docker/run.sh
+++ notejam-docker/run.sh
@@ -1,3 +1,34 @@
 #!/bin/sh
 
-# TODO write a shell script to automate the build and run process
+printf "Stop and remove existing containers"
+docker-compose down -v || {
+    printf "Failed to stop containers. Check Docker status."
+    exit 1
+}
+
+printf "Removing redundant resources"
+docker system prune -af || {
+    printf "Failed to clean Docker resources."
+    exit 1
+}
+
+printf "Build and run all docker containers"
+docker-compose up --build -d || {
+    printf "Failed to build or start containers."
+    exit 1
+}
+
+sleep 5
+
+printf "Running database migrations"
+docker exec -it notejam-app python manage.py syncdb || {
+    printf "Failed to run syncdb."
+    exit 1
+}
+
+docker exec -it notejam-app python manage.py migrate || {
+    printf "Failed to run migrations."
+    exit 1
+}
+
+printf "Application is now running."
